0529a8fe3e56503bbb844db68fdc1c433c067b0c
Shirley <shirleyzhu@college.harvard.edu>
2022-11-06 21:38:25 -0500

    add constraint to center pickerview on x anchor

--- WaterLogging/WaterLogging/TrackWaterViewController.swift
+++ WaterLogging/WaterLogging/TrackWaterViewController.swift
@@ -6,17 +6,24 @@
 
 import UIKit
 
-class TrackWaterViewController: UIViewController {
+class TrackWaterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     
     private let addWaterButton = UIButton()
     private let updateGoalButton = UIButton()
+    private let updateGoalDropdown = UIPickerView()
+    let list = [1, 2, 3]
     let defaults = UserDefaults.standard
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setUp()
     }
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateGoalDropdown.delegate = self
+        updateGoalDropdown.dataSource = self
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -41,8 +48,10 @@ class TrackWaterViewController: UIViewController {
         let container = UIView()
         addWaterButton.translatesAutoresizingMaskIntoConstraints = false
         updateGoalButton.translatesAutoresizingMaskIntoConstraints = false
+        updateGoalDropdown.isHidden = true
         container.addSubview(addWaterButton)
         container.addSubview(updateGoalButton)
+        container.addSubview(updateGoalDropdown)
         container.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(container)
@@ -60,6 +69,10 @@ class TrackWaterViewController: UIViewController {
                                            updateGoalButton.bottomAnchor.constraint(equalTo: container.bottomAnchor)]
         
         NSLayoutConstraint.activate(updateGoalButtonConstraints)
+        
+        let updateGoalDropdownConstraints = [updateGoalDropdown.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)]
+
+        NSLayoutConstraint.activate(updateGoalDropdownConstraints)
         
         // ContainerView constraints
         
@@ -86,8 +99,25 @@ class TrackWaterViewController: UIViewController {
     
     @objc private func goalButtonPressed() {
         print("Goal button pressed")
+        updateGoalDropdown.isHidden = false
     }
     
+    // Picker View
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(list[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        updateGoalDropdown.isHidden = true;
+    }
 
 }
 
