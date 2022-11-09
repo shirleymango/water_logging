850e04e9dfb22b62780235c0530cf69ad0d16cdb
Shirley <shirleyzhu@college.harvard.edu>
2022-11-09 03:34:25 -0500

    add to tear down methods

--- WaterLogging/WaterLoggingTests/TrackWaterViewControllerTests.swift
+++ WaterLogging/WaterLoggingTests/TrackWaterViewControllerTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import WaterLogging
 
 class TrackWaterViewControllerTests: XCTestCase {
-    let sut = TrackWaterViewController()
+    var sut = TrackWaterViewController()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,6 +19,7 @@ class TrackWaterViewControllerTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = TrackWaterViewController()
     }
     
     func testInitialValues() {
--- WaterLogging/WaterLoggingTests/VisualizeWaterIntakeViewControllerTests.swift
+++ WaterLogging/WaterLoggingTests/VisualizeWaterIntakeViewControllerTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import WaterLogging
 
 class VisualizeWaterIntakeViewControllerTests: XCTestCase {
-    let sut = VisualizeWaterIntakeViewController()
+    var sut = VisualizeWaterIntakeViewController()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,6 +19,7 @@ class VisualizeWaterIntakeViewControllerTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = VisualizeWaterIntakeViewController()
     }
     
     func testInitialValues() {
